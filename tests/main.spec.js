da05c05f65f86d7e8789d33decee0287a372c5c4
Bruno Romeiro <bruno.romeiro@gmail.com>
2020-06-03 18:22:49 -0300

    feat: 🎸 Implements Smoke Tests

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,5 +1,32 @@
 import { expect } from 'chai';
+import {
+  search,
+  searchAlbums,
+  searchArtists,
+  searchTracks,
+  searchPlaylist,
+} from '../src/main';
 
-describe('Main', () => {
+describe('Spotify Wrapper', () => {
+
+  describe('Smoke Tests', () => {
+    it('should exists the search method', () => {
+      expect(search).to.exist;
+    });
+    it('should exists the searchAlbums method', () => {
+      expect(searchAlbums).to.exist;
+    });
+    it('should exists the searchArtists method', () => {
+      expect(searchArtists).to.exist;
+    });
+    it('should exists the searchTracks method', () => {
+      expect(searchTracks).to.exist;
+    });
+    it('should exists the searchPlaylist method', () => {
+      expect(searchPlaylist).to.exist;
+    });
+
+
+  });
 
 });
